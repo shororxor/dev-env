8dfb7da3c142a4a314f9b856ff5c2f97179b9f88
Bao Nguyen <bao.nguyen@u.nus.edu>
2026-08-30 00:26:12 +0800

    Remove listchar nvim

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -18,7 +18,7 @@ vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
